021d40993c1d774710351e40771f1956ca8a953d
Emma Piers <emmapiers4@gmail.com>
2024-08-14 21:20:21 -0400

    fix given info

--- nfl_scraper.py
+++ nfl_scraper.py
@@ -2,8 +2,72 @@ import requests
 import pickle
 from bs4 import BeautifulSoup as bs
 
-url = "https://www.nfl.com/stats/player-stats/"
+from selenium import webdriver
+import time
 
+url = "https://www.nfl.com/stats/player-stats/"
+url2 = "https://www.pro-football-reference.com/years/2023/opp.htm"
+
+def scrape_page2(url2):
+
+    driver = webdriver.Safari()
+
+    driver.get(url2)
+    driver.implicitly_wait(10)
+    
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+
+    table = soup.find('table', {'id': 'team_stats'})
+    if not table: 
+        print("table not found")
+        return 
+    
+    header_row = table.find('tr', class_=lambda x: x != 'over_header')
+
+    # Extract the headers from that 'tr'
+    headers = [th.getText().strip() for th in header_row.find_all('th')]
+
+    plays_index = headers.index("Ply")
+    games_index = headers.index("G")
+    passes_index = headers.index("Att")
+
+    att_indices = [i for i, header in enumerate(headers) if header == "Att"]
+    if len(att_indices) > 1:
+        rushes_index = att_indices[1]
+
+    rows = table.find_all('tr')[2:]  # Skip the first two rows (over-headers and actual headers)
+    plays_stats = []
+
+    for row in rows[:-3]:
+        columns = row.find_all('td')
+        if columns:
+            team_name = columns[0].getText().strip()
+            games_played = columns[games_index-1].getText().strip()
+            plays = columns[plays_index-1].getText().strip()
+            passes = columns[passes_index-1].getText().strip()
+            rushes = columns[rushes_index -1].getText().strip()
+           
+            games_played_int = int(games_played)
+            passes_int = int(passes)
+            rushes_int = int(rushes)
+
+            plays_int = float(plays)
+
+            plays_per_game = round(plays_int / games_played_int, 1)
+            passes_per_game = round(passes_int / games_played_int, 3)
+            pass_percentage_per_game = round((passes_per_game / plays_per_game) * 100, 3)
+            rushes_per_game = round(rushes_int / games_played_int, 3)
+            rushes_percentage_per_game = round((rushes_per_game / plays_per_game) * 100, 3)
+            
+            plays_stats.append({"Team": team_name, "Plays/G": str(plays_per_game), "Pass %/G": str(pass_percentage_per_game), "Rush %/G": str(rushes_percentage_per_game)})
+
+    # Print the extracted "Plays" stats
+    for stat in plays_stats:
+        print(stat)
+
+    # Close the browser
+    driver.quit()
 
 
 def scrape_page(url):
@@ -46,17 +110,19 @@ def get_next_page(url):
 
 
 def main():
-    base_url = 'https://www.nfl.com/stats/player-stats/category/passing/2023/REG/all/passingyards/DESC'
+   scrape_page2(url2)
+   
+   # base_url = 'https://www.nfl.com/stats/player-stats/category/passing/2023/REG/all/passingyards/DESC'
 
-    all_data = []
-    url = base_url
-    while url:
-        page_data = scrape_page(url)
-        all_data.extend(page_data)
-        url = get_next_page(url)
+    #all_data = []
+   # url = base_url
+   # while url:
+   #     page_data = scrape_page(url)
+   #     all_data.extend(page_data)
+   #     url = get_next_page(url)
 
-    with open('all_data.pkl', 'wb') as file:
-        pickle.dump(all_data, file)
+   # with open('all_data.pkl', 'wb') as file:
+   #     pickle.dump(all_data, file)
     
 
 if __name__ == "__main__":
